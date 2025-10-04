abc5a7c0bb76b0bbd93d37f5a23cb96673b183c1
thefrancho <sebastian.franco@utp.edu.co>
2025-10-04 16:15:50 -0500

    add utils file

---
+++ src/ai_factory/utils.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+
+
+def get_file_list(folder_path: Path):
+    files_list = os.listdir(folder_path)
+    files_path = sorted(
+        [
+            os.path.join(folder_path, file_path)
+            for file_path in files_list
+            if file_path.lower()
+        ]
+    )
+    return files_path
