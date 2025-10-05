812456fbbcf491fdc17a7aad8737cdbe9af29dec
thefrancho <sebastian.franco@utp.edu.co>
2025-10-04 21:32:03 -0500

    minor utils fix

--- src/ai_factory/utils.py
+++ src/ai_factory/utils.py
@@ -5,10 +5,6 @@ from pathlib import Path
 def get_file_list(folder_path: Path):
     files_list = os.listdir(folder_path)
     files_path = sorted(
-        [
-            os.path.join(folder_path, file_path)
-            for file_path in files_list
-            if file_path.lower()
-        ]
+        [os.path.join(folder_path, file_path) for file_path in files_list]
     )
     return files_path
