cf46868a27c4831245d44ec58df2a9f9564488e6
thefrancho <sebastian.franco@utp.edu.co>
2025-10-05 23:16:46 -0500

    create agents form factory instead of using singleton

--- src/ai_factory/agents/cv_extracter/orchestrator/orchestrator.py
+++ src/ai_factory/agents/cv_extracter/orchestrator/orchestrator.py
@@ -1,3 +1,4 @@
+import json
 import os
 from pathlib import Path
 import asyncio
@@ -10,15 +11,14 @@ from ai_factory.utils import get_file_list
 from ai_factory.agents.cv_extracter.orchestrator.plan import build_overall_workflow
 
 
-async def run_over_folder(folder: Path):
-    app_name = "ai-factory"
-    user_id = "thefrancho"
-    session_service = InMemorySessionService()
+async def run_over_folder(file_path: str, sem: asyncio.Semaphore):
+    async with sem:
+        app_name = "ai-factory"
+        user_id = "thefrancho"
+        session_service = InMemorySessionService()
 
-    overall_workflow = build_overall_workflow()
+        overall_workflow = build_overall_workflow()
 
-    files = [p for p in get_file_list(folder) if not os.path.isdir(p)]
-    for file_path in files:
         session = await session_service.create_session(
             app_name=app_name, user_id=user_id
         )
@@ -44,33 +44,42 @@ async def run_over_folder(folder: Path):
             )
         ).state
 
-        print(f"\n=== {os.path.basename(file_path)} ===")
-        print(state["title_section"])  # output of cv_title_pattern_agent
-        print("-" * 60)
-        print(state["filename_pattern_section"])  # output of cv_filename_pattern_agent
-        print("-" * 60)
-        print(
-            state["file_processing_pattern_section"]
-        )  # output of cv_filename_pattern_agent
-        print("-" * 60)
-        print(
-            state["volume_characteristics_section"]
-        )  # output of cv_filename_pattern_agent
-        print("-" * 60)
-        print(
-            state["day_of_week_section_pattern"]
-        )  # output of cv_filename_pattern_agent
-        print("-" * 60)
-        print(
-            state["recurring_patterns_section"]
-        )  # output of cv_filename_pattern_agent
-        print("-" * 60)
-        print(
-            state["comments_for_analyst_section"]
-        )  # output of cv_filename_pattern_agent
-        print("-" * 60)
-        print(f"\n=== End Of File ===")
+        full_extraction_sections = {
+            "title_section": state["title_section"],
+            "filename_pattern_section": state["filename_pattern_section"],
+            "file_processing_pattern_section": state["file_processing_pattern_section"],
+            "volume_characteristics_section": state["volume_characteristics_section"],
+            "day_of_week_section_pattern": state["day_of_week_section_pattern"],
+            "recurring_patterns_section": state["recurring_patterns_section"],
+            "comments_for_analyst_section": state["comments_for_analyst_section"],
+        }
+
+        output_dir = "custom_outputs/complete_sections"
+
+        os.makedirs(output_dir, exist_ok=True)
+        out_path = os.path.join(output_dir, f"{os.path.basename(file_path)}.json")
+        with open(out_path, "w", encoding="utf-8") as fp:
+            json.dump(full_extraction_sections, fp, ensure_ascii=False, indent=4)
 
 
-folder = "dataset_files/datasource_cvs"
-asyncio.run(run_over_folder(folder))
+async def main():
+    CONCURRENCY = 3
+    folder = "dataset_files/datasource_cvs"
+    sem = asyncio.Semaphore(CONCURRENCY)
+
+    folder_path = Path(folder)
+    files_path = [p for p in get_file_list(folder_path) if not os.path.isdir(p)]
+
+    tasks = [
+        asyncio.create_task(run_over_folder(file_path=fp, sem=sem)) for fp in files_path
+    ]
+
+    for coro in asyncio.as_completed(tasks):
+        try:
+            await coro
+        except Exception as e:
+            print(f"Task failed: {e!r}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
